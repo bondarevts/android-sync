44b7c3c2323882df9ea986ad9577e61521db8abc
Timofei Bondarev <bondarevts@gmail.com>
2020-10-09 16:42:20 -0400

    Extract commands into separate functions

--- src/android_sync/cli.py
+++ src/android_sync/cli.py
@@ -13,13 +13,13 @@ def parse_args() -> argparse.Namespace:
 
     subparsers.add_parser('all')
 
-    month_command = subparsers.add_parser('month')
-    month_command.add_argument('--move', action='store_true')
-    month_command.add_argument('month', help='YYYY-MM', action='store', nargs='?')
+    month_parser = subparsers.add_parser('month')
+    month_parser.add_argument('--move', action='store_true')
+    month_parser.add_argument('month', help='YYYY-MM', action='store', nargs='?')
 
-    folder_command = subparsers.add_parser('folder')
-    folder_command.add_argument('device_path')
-    folder_command.add_argument('target_path')
+    folder_parser = subparsers.add_parser('folder')
+    folder_parser.add_argument('device_path')
+    folder_parser.add_argument('target_path')
     return parser.parse_args()
 
 
@@ -32,31 +32,39 @@ def should_retry(answer):
     return not answer.lower().startswith('n')
 
 
+def folder_command(args):
+    sync_folder(args.device_path, args.target_path)
+
+
+def month_command(args):
+    if args.month is None:
+        month = Month.current()
+    else:
+        month = parse_month(args.month)
+
+    answer = ''
+    while should_retry(answer):
+        try:
+            sync_month(month, clean=args.move)
+        except NoDeviceException:
+            answer = input('Device is not found. Please plug in the device.\nRetry [Y/n]? ')
+        except UnauthorizedException:
+            answer = input('Connection is not authorized. Please check the dialog on the device screen. '
+                           'Retry [Y/n]? ')
+        else:
+            answer = 'no'
+
+
 def main() -> None:
     args = parse_args()
     if args.command == 'all':
         raise Exception('Not implemented yet')
 
     if args.command == 'month':
-        if args.month is None:
-            month = Month.current()
-        else:
-            month = parse_month(args.month)
-
-        answer = ''
-        while should_retry(answer):
-            try:
-                sync_month(month, clean=args.move)
-            except NoDeviceException:
-                answer = input('Device is not found. Please plug in the device.\nRetry [Y/n]? ')
-            except UnauthorizedException:
-                answer = input('Connection is not authorized. Please check the dialog on the device screen. '
-                               'Retry [Y/n]? ')
-            else:
-                answer = 'no'
+        month_command(args)
 
     if args.command == 'folder':
-        sync_folder(args.device_path, args.target_path)
+        folder_command(args)
 
 
 if __name__ == '__main__':
